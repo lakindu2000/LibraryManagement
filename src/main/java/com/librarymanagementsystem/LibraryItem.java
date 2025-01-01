e459f0108f21f11e8ee8ee6d66a3ef2f8834c17f
lakindu <lakinduedirisinghe2000@gmail.com>
2025-01-01 04:01:42 -0800

    initial commit

---
+++ src/main/java/com/librarymanagementsystem/LibraryItem.java
@@ -0,0 +1,34 @@
+package com.librarymanagementsystem;
+
+import java.io.Serializable;
+
+public abstract class LibraryItem implements Serializable {
+    private String title;
+    private String author;
+    private String serialNumber;
+    protected boolean isBorrowed;
+    public abstract void borrowedItem(User user);
+
+    public LibraryItem(String title, String author, String serialNumber) {
+        this.title = title;
+        this.author = author;
+        this.serialNumber = serialNumber;
+        this.isBorrowed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+}
